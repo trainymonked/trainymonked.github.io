89d9ba54d95a9382101910d71a4fe398671e958c
trainymonked <trainymonked@gmail.com>
2022-11-07 06:38:03 +0300

    -g

--- js/js.js
+++ js/js.js
@@ -2,6 +2,10 @@ document.getElementById('batman').addEventListener('click',
   () => window.open('../batman/index.html', '_blank'));
 document.getElementById('yoga-india').addEventListener('click',
   () => window.open('../yoga-india/index.html', '_blank'));
+document.getElementById('tetris').addEventListener('click',
+  () => window.open('../tetris/index.html', '_blank'));
+document.getElementById('pixi-figures').addEventListener('click',
+  () => window.open('../pixi-figures/index.html', '_blank'));
 
 document.querySelectorAll('.list__item')
   .forEach(i => {
