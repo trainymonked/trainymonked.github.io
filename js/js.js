b50d9b67a17508b15b0a54e5deb624daff57f5f4
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-01-25 02:47:40 +0300

    added todo

--- js/js.js
+++ js/js.js
@@ -1,18 +1,18 @@
-document.getElementById('batman').addEventListener('click',
-  () => window.open('https://trainymonked.github.io/batman', '_blank'));
-document.getElementById('yoga-india').addEventListener('click',
-  () => window.open('https://trainymonked.github.io/yoga-india', '_blank'));
-document.getElementById('tetris').addEventListener('click',
-  () => window.open('https://trainymonked.github.io/tetris', '_blank'));
-document.getElementById('pixi-figures').addEventListener('click',
-  () => window.open('https://trainymonked.github.io/pixi-figures', '_blank'));
+const projects = ['yoga-india', 'todo', 'pixi-figures', 'batman', 'tetris']
 
-document.querySelectorAll('.list__item')
-  .forEach(i => {
-    i.addEventListener('mouseenter', e => {
-      e.target.firstElementChild.style.opacity = 0.5;
-    });
-    i.addEventListener('mouseleave', e => {
-      e.target.firstElementChild.style.opacity = 1;
-    });
-  });
+projects.forEach((project) => {
+  document
+    .getElementById(project)
+    .addEventListener('click', () =>
+      window.open(`https://trainymonked.github.io/${project}`, '_blank')
+    )
+})
+
+document.querySelectorAll('.list__item').forEach((i) => {
+  i.addEventListener('mouseenter', (e) => {
+    e.target.firstElementChild.style.opacity = 0.5
+  })
+  i.addEventListener('mouseleave', (e) => {
+    e.target.firstElementChild.style.opacity = 1
+  })
+})
